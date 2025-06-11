aae9c13b1c998fdc210fa1efdcffe97e29ae8994
RocketTheRacoon <57435847+Horizonan@users.noreply.github.com>
2025-06-11 06:43:43 +0000

    Restored to 'd46a426f7fc46db70d20b2c76d978fe7ea91c3a3'
    Replit-Restored-To: d46a426f7fc46db70d20b2c76d978fe7ea91c3a3

--- src/stores/Store.jsx
+++ src/stores/Store.jsx
@@ -324,7 +324,10 @@ export default function Store({
               <h3 style={{ color: "#9400D3", textAlign: "center" }}>
                 First Ascension
               </h3>
-              {renderItems(firstAsc)}
+              <div className="store-items">
+                {renderItems(firstAsc)}
+
+              </div>
             </div>
           )}
       </div>
